40bdc42c65142cfd496f35b3ab347da882f08572
Aaron Johnson <acjohnson@pcdomain.com>
2017-03-29 23:50:56 -0500

    added optional email field

--- address_label_generator.py
+++ address_label_generator.py
@@ -5,7 +5,12 @@ import yaml
 def _yaml_to_csv(name=None, address=None):
     # CSV Headers
     #First Name,Last Name,Home Street,Home Street 2,Home City,Home State,Home Postal Code,Home Country,E-mail Address
-    return(name + ',,' + address['street'] + ',,' + address['city'] + ',' + address['state'] + ',' + address['zip'] + ',,')
+    if 'email' in address:
+        email = address['email']
+    else:
+        email = ''
+
+    return(name + ',,' + address['street'] + ',,' + address['city'] + ',' + address['state'] + ',' + address['zip'] + ',' + email + ',')
 
 def load_guest_list():
     with open("addresses.yaml", 'r') as stream:
