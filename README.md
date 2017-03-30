1cf1246364a01e1a63183f0c23c8b22453a69d3b
Aaron Johnson <acjohnson@pcdomain.com>
2017-03-29 22:00:37 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Address Label generator compatible with Shutterfly
---
+++ address_label_generator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import yaml
+
+def _yaml_to_csv(name=None, address=None):
+    # CSV Headers
+    #First Name,Last Name,Home Street,Home Street 2,Home City,Home State,Home Postal Code,Home Country,E-mail Address
+    return(name + ',,' + address['street'] + ',,' + address['city'] + ',' + address['state'] + ',' + address['zip'] + ',,')
+
+def load_guest_list():
+    with open("addresses.yaml", 'r') as stream:
+        try:
+            party = yaml.load(stream)
+            return(party)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+def generate_labels(guest_list=None): 
+    # Create CSV
+    for name, address in guest_list.iteritems():
+        print(_yaml_to_csv(name, address))
+
+def main():
+    party=load_guest_list()
+    print("First Name,Last Name,Home Street,Home Street 2,Home City,Home State,Home Postal Code,Home Country,E-mail Address")
+    generate_labels(party['guest_list']['family'])
+    generate_labels(party['guest_list']['friends'])
+
+if __name__ == "__main__":
+    main()
